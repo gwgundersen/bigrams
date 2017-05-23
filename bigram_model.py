9894d40eab5c9e562cfee423d18e6c8057a29417
Greg <ggundersen@gmail.com>
2017-05-23 12:06:06 -0400

    Refactored into Bigram class.

---
+++ bigram_model.py
@@ -0,0 +1,128 @@
+"""Bigram model:
+
+https://en.wikipedia.org/wiki/Bigram
+"""
+
+import nltk
+import random
+
+
+class BigramModel(object):
+
+    START = '<s>'
+    STOP = '</s>'
+
+# Public methods
+# -----------------------------------------------------------------------------
+
+    def __init__(self):
+        pass
+
+    def fit(self, corpus):
+        self.unigram_c   = self._calc_unigrams(corpus)
+        self.bigram_p    = self._calc_bigram_p(corpus)
+        self.start_words = self._get_start_words()
+        self.stop_words  = self._get_stop_words()
+
+    def gen_sentence(self, max_length=10, start_word=None, num_top_words=3):
+        if start_word:
+            w = start_word
+        else:
+            r = random.randint(0, len(self.start_words)-1)
+            w = self.start_words[r]
+        sentence = [w]
+        while True:
+            w = self._gen_next_word(self.bigram_p, w, num_top_words)
+            if w is None:
+                break
+            sentence.append(w)
+            if w in self.stop_words:
+                if random.random() < 0.25 or len(sentence) >= max_length:
+                    break
+        sentence = [s for s in sentence if s != self.START and s != self.STOP]
+        return ' '.join(sentence)
+
+# Private methods
+# -----------------------------------------------------------------------------
+
+    def _gen_next_word(self, bigram_p, w0, num_top_words):
+        probs = []
+        for b, p in bigram_p.items():
+            w1, w2 = b
+            if w1 == w0:
+                probs.append((w2, p))
+        if len(probs) == 0:
+            return None
+        top_words = sorted(probs, key=lambda x: x[1])[:num_top_words]
+        r = random.randint(0, len(top_words)-1)
+        return top_words[r][0]
+
+    def _calc_unigrams(self, corpus):
+        unigram_c = {}
+        for sentence in corpus:
+            for token in _tokenize(sentence):
+                if token not in unigram_c:
+                    unigram_c[token] = 0
+                unigram_c[token] += 1
+        unigram_c[self.START] = len(corpus)
+        return unigram_c
+
+    def _calc_bigram_p(self, corpus):
+        temp = {}
+        for sentence in corpus:
+            tokens = _tokenize(sentence)
+            tokens = [self.START] + tokens + [self.STOP]
+            bigrams = nltk.bigrams(tokens)
+            for b in bigrams:
+                if b not in temp:
+                    temp[b] = 0
+                temp[b] += 1
+
+        bigram_p = {}
+        for token, count in temp.items():
+            w1, w2 = token
+            numer = count
+            denom = self.unigram_c[w1]
+            bigram_p[token] = numer / float(denom)
+
+        self._validate_bigram_p(bigram_p)
+        return bigram_p
+
+    def _get_start_words(self):
+        words = []
+        for b, p in self.bigram_p.items():
+            w1, w2 = b
+            if w1 == self.START:
+                words.append(w2)
+        return words
+
+    def _get_stop_words(self):
+        words = []
+        for b, p in self.bigram_p.items():
+            w1, w2 = b
+            if w2 == self.STOP:
+                words.append(w1)
+        return words
+
+    def _get_start_stop_words(self, symbol):
+        words = []
+        for b, p in self.bigram_p.items():
+            w1, w2 = b
+            if w1 == symbol:
+                words.append(w2)
+        return words
+
+    def _validate_bigram_p(self, bigram_p):
+        vocab = set(self.unigram_c.keys())
+        vocab.add(self.START)
+        for v in vocab:
+            s = 0
+            for b, p in bigram_p.items():
+                if b[0] == v:
+                    s += p
+            assert abs(1 - s) < 1e-9
+        self.vocab = vocab
+
+
+def _tokenize(data):
+    return data.split()
--- lessons.py
+++ lessons.py
@@ -3,104 +3,25 @@
 
 
 import csv
-import nltk
+from bigram_model import BigramModel
 import random
 import string
 
-START = '<s>'
-STOP = '</s>'
-
 
 def main():
-    corpus = clean(load_data())
-    unigram_c = calc_unigrams(corpus)
-    bigram_p = calc_bigram_p(corpus, unigram_c)
-    gen_sentence(bigram_p, 10)
+    corpus = load_data()
 
+    bm = BigramModel()
+    bm.fit(corpus)
 
-def gen_sentence(bigram_p, length):
-    start_words = []
-    for b, p in bigram_p.items():
-        w1, w2 = b
-        if w1 == START:
-            start_words.append(w2)
-    for w in start_words:
-        sentence = [w]
-        for i in range(length-1):
-            w = get_next_word(bigram_p, w)
-            sentence.append(w)
-        sentence = [s for s in sentence if s and s != START and s != STOP]
-        print(' '.join(sentence))
-
-
-def get_next_word(bigram_p, w0, num_top_words=3):
-    probs = []
-    for b, p in bigram_p.items():
-        w1, w2 = b
-        if w1 == w0:
-            probs.append((w2, p))
-    if len(probs) == 0:
-        return None
-    top_words = sorted(probs, key=lambda x: x[1])[:num_top_words]
-    r = random.randint(0, len(top_words)-1)
-    return top_words[r][0]
-
-
-def calc_unigrams(corpus):
-    unigram_c = {}
-    for sentence in corpus:
-        for token in tokenize(sentence):
-            if token not in unigram_c:
-                unigram_c[token] = 0
-            unigram_c[token] += 1
-    unigram_c[START] = len(corpus)
-    return unigram_c
-
-
-def calc_bigram_p(corpus, unigram_c):
-    temp = {}
-    for sentence in corpus:
-        tokens = tokenize(sentence)
-        tokens = [START] + tokens + [STOP]
-        bigrams = nltk.bigrams(tokens)
-        for b in bigrams:
-            if b not in temp:
-                temp[b] = 0
-            temp[b] += 1
-
-    bigram_p = {}
-    for token, count in temp.items():
-        w1, w2 = token
-        numer = count
-        denom = unigram_c[w1]
-        bigram_p[token] = numer / float(denom)
-
-    validate_bigram_p(bigram_p, unigram_c)
-    return bigram_p
-
-
-def validate_bigram_p(bigram_p, unigram_c):
-    vocab = set(unigram_c.keys())
-    vocab.add(START)
-    for v in vocab:
-        s = 0
-        for b, p in bigram_p.items():
-            if b[0] == v:
-                s += p
-        assert abs(1 - s) < 1e-9
-
-
-def clean(data):
-    result = []
-    for line in data:
-        line = line.translate(None, string.punctuation)
-        line = line.lower()
-        result.append(line)
-    return result
-
-
-def tokenize(data):
-    return data.split()
+    with open('fake_lessons.txt', 'w+') as out:
+        for _ in range(1000):
+            print(_)
+            r1 = random.randint(10, 30)
+            r2 = random.randint(2, 5)
+            sentence = bm.gen_sentence(max_length=r1, start_word=None,
+                                       num_top_words=r2)
+            out.write(sentence + '\n')
 
 
 def load_data():
@@ -110,7 +31,10 @@ def load_data():
         for line in r:
             if len(line) > 1:
                 raise AttributeError('Data format is incorrect.')
-            result.append(line[0].strip())
+            line = line[0].strip()
+            line = line.translate(None, string.punctuation)
+            line = line.lower()
+            result.append(line)
     return result
 
 
